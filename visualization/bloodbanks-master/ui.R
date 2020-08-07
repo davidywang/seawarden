78cce152d12874f41d44efeed2f163f4ce18f8b0
unknown <zack_dinh@berkeley.edu>
2020-08-06 23:02:32 -0700

    med

--- visualization/bloodbanks-master/ui.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-library(leaflet)
-
-navbarPage("Location of Blood Banks", id="main",
-           tabPanel("Map", leafletOutput("bbmap", height=1000)),
-           tabPanel("Data", DT::dataTableOutput("data")),
-           tabPanel("Read Me",includeMarkdown("readme.md")))
-
--- visualization/sw_map/server.R
+++ visualization/sw_map/server.R
@@ -20,7 +20,7 @@ shinyServer(function(input, output){
   grc_frm <- geojson_read(paste0(src, f, "/", f, frm), what = "sp")
   grc_pen <- geojson_read(paste0(src, f, "/", f, pen), what = "sp")
   grc_pts <- geojson_read(paste0(src, f, "/", f, pts), what = "sp")
-  
+
   f = "TUR"
   tur_aoi <- geojson_read(paste0(src_aoi, f, aoi),  what = "sp")
   tur_frm <- geojson_read(paste0(src, f, "/", f, frm), what = "sp")
@@ -83,7 +83,7 @@ shinyServer(function(input, output){
       addPolygons(data=grc_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(grc_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
       addPolygons(data=grc_frm, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(grc_frm, feature.id = FALSE, row.numbers = FALSE)) %>%
       addPolygons(data=grc_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(grc_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
-      #addCircleMarkers(data=grc_pts, radius = 1, color = '#3bb2d0', popup = paste0('<a href="', src_img, grc_pts$filename,'">View Image</a>')) #%>%
+      #addCircleMarkers(data=grc_pts, radius = 1, color = '#3bb2d0', popup = paste0('<a href="', src_img, grc_pts$filename,'">View Image</a>')) %>%
       addCircleMarkers(data=grc_pts, radius = 1, color = '#3bb2d0', popup = paste0(
         paste0(' Farm Site: ', grc_pts$farm.ID, ' <a href="', src_img, grc_pts$filename,'" target="_blank"','">View Image</a>'), 
         popupImage(paste0(src_img, grc_pts$filename), src = "remote", height=300, width=300))) %>%
@@ -93,7 +93,7 @@ shinyServer(function(input, output){
       addPolygons(data=tur_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(tur_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=tur_pts, radius = 1, color = '#3bb2d0', popup = popupTable(tur_pts)) %>%
       addCircleMarkers(data=tur_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', tur_pts$farm.ID, ' <a href="', src_img, tur_pts$filename,'" target="_blank"','">View Image</a>'),  
+        paste0('Farm Site: ', tur_pts$farm.ID, ' <a href="', src_img, tur_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, tur_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=hrv_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(hrv_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -101,7 +101,7 @@ shinyServer(function(input, output){
       addPolygons(data=hrv_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(hrv_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=hrv_pts, radius = 1, color = '#3bb2d0', popup = popupTable(hrv_pts)) %>%
       addCircleMarkers(data=hrv_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', hrv_pts$farm.ID, ' <a href="', src_img, hrv_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', hrv_pts$farm.ID, ' <a href="', src_img, hrv_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, hrv_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=cyp_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(cyp_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -109,7 +109,7 @@ shinyServer(function(input, output){
       addPolygons(data=cyp_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(cyp_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=cyp_pts, radius = 1, color = '#3bb2d0', popup = popupTable(cyp_pts)) %>%
       addCircleMarkers(data=cyp_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', cyp_pts$farm.ID, ' <a href="', src_img, cyp_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', cyp_pts$farm.ID, ' <a href="', src_img, cyp_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, cyp_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=alb_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(alb_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -117,7 +117,7 @@ shinyServer(function(input, output){
       addPolygons(data=alb_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(alb_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=alb_pts, radius = 1, color = '#3bb2d0', popup = popupTable(alb_pts)) %>%
       addCircleMarkers(data=alb_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', alb_pts$farm.ID, ' <a href="', src_img, alb_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', alb_pts$farm.ID, ' <a href="', src_img, alb_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, alb_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=ita_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(ita_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -125,7 +125,7 @@ shinyServer(function(input, output){
       addPolygons(data=ita_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(ita_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=ita_pts, radius = 1, color = '#3bb2d0', popup = popupTable(ita_pts)) %>%
       addCircleMarkers(data=ita_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', ita_pts$farm.ID, ' <a href="', src_img, ita_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', ita_pts$farm.ID, ' <a href="', src_img, ita_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, ita_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=esp_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(esp_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -133,7 +133,7 @@ shinyServer(function(input, output){
       addPolygons(data=esp_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(esp_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=esp_pts, radius = 1, color = '#3bb2d0', popup = popupTable(esp_pts)) %>%
       addCircleMarkers(data=esp_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', esp_pts$farm.ID, ' <a href="', src_img, esp_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', esp_pts$farm.ID, ' <a href="', src_img, esp_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, esp_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=mlt_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(mlt_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -141,7 +141,7 @@ shinyServer(function(input, output){
       addPolygons(data=mlt_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(mlt_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=mlt_pts, radius = 1, color = '#3bb2d0', popup = popupTable(mlt_pts)) %>%
       addCircleMarkers(data=mlt_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', mlt_pts$farm.ID, ' <a href="', src_img, mlt_pts$filename,'" target="_blank"','">View Image</a>'),  
+        paste0('Farm Site: ', mlt_pts$farm.ID, ' <a href="', src_img, mlt_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, mlt_pts$filename), src = "remote", height=300, width=300))) %>%
 
       addPolygons(data=fra_aoi, color = 'white', weight = 1, fillOpacity = 0, popup = popupTable(fra_aoi, feature.id = FALSE, row.numbers = FALSE)) %>%
@@ -149,7 +149,7 @@ shinyServer(function(input, output){
       addPolygons(data=fra_pen, color = '#3bb2d0', weight = 2, fillOpacity = 0, popup = popupTable(fra_pen, feature.id = FALSE, row.numbers = FALSE)) %>%
       # addCircleMarkers(data=fra_pts, radius = 1, color = '#3bb2d0', popup = popupTable(fra_pts))
       addCircleMarkers(data=fra_pts, radius = 1, color = '#3bb2d0', popup = paste0(
-        paste0('Farm Site: ', fra_pts$farm.ID, ' <a href="', src_img, fra_pts$filename,'" target="_blank"','">View Image</a>'), 
+        paste0('Farm Site: ', fra_pts$farm.ID, ' <a href="', src_img, fra_pts$filename,'" target="_blank"','">View Image</a>'),
         popupImage(paste0(src_img, fra_pts$filename), src = "remote", height=300, width=300)))
     })
 })
